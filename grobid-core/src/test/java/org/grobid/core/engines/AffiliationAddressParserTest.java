aff63a40dbc4db724046cdd53cc80cbe292b476b
Daniel Ecer <de-code@users.noreply.github.com>
2020-02-28 13:10:56 +0000

    added shouldExtractSimpleAffiliation

--- grobid-core/src/test/java/org/grobid/core/engines/AffiliationAddressParserTest.java
+++ grobid-core/src/test/java/org/grobid/core/engines/AffiliationAddressParserTest.java
@@ -4,32 +4,47 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.AfterClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import com.google.common.base.Joiner;
+
+import org.grobid.core.analyzers.GrobidAnalyzer;
 import org.grobid.core.data.Affiliation;
 import org.grobid.core.factory.GrobidFactory;
+import org.grobid.core.features.FeaturesVectorAffiliationAddress;
 import org.grobid.core.layout.LayoutToken;
 import org.grobid.core.main.LibraryLoader;
 import org.grobid.core.utilities.GrobidProperties;
-import org.hamcrest.collection.IsEmptyCollection;
+import org.grobid.core.utilities.OffsetPosition;
 
 
 public class AffiliationAddressParserTest {
 
+	public static final Logger LOGGER = LoggerFactory.getLogger(AffiliationAddressParserTest.class);
+
     private static boolean NO_USE_PRELABEL = false;
+    private static List<List<OffsetPosition>> NO_PLACES_POSITIONS = Arrays.asList(
+        Collections.emptyList()
+    );
 
     private AffiliationAddressParser target;
+    private GrobidAnalyzer analyzer;
 
     @Before
     public void setUp() throws Exception {
-        target = new AffiliationAddressParser();
+        this.target = new AffiliationAddressParser();
+        this.analyzer = GrobidAnalyzer.getInstance();
     }
 
     @BeforeClass
@@ -52,7 +67,47 @@ public class AffiliationAddressParserTest {
             tokenizations,
             NO_USE_PRELABEL
         );
-        assertThat("affiliations should be empty", result, is(not(empty())));
+        assertThat("affiliations should be null", result, is(nullValue()));
     }
 
+    private static String addLabelsToFeatures(String header, List<String> labels) {
+        String[] headerLines = header.split("\n");
+        if (headerLines.length != labels.size()) {
+            throw new IllegalArgumentException(String.format(
+                "number of header lines and labels must match, %d != %d",
+                headerLines.length, labels.size()
+            ));
+        }
+        ArrayList<String> resultLines = new ArrayList<>(headerLines.length);
+        for (int i = 0; i < headerLines.length; i++) {
+            resultLines.add(headerLines[i] + " " + labels.get(i));
+        }
+        return Joiner.on("\n").join(resultLines);
+    }
+
+    @Test
+    public void shouldExtractSimpleAffiliation() throws Exception {
+        List<String> tokens = Arrays.asList("1", " ", "University", " ", "of", " ", "Science");
+        List<String> labels = Arrays.asList("I-<marker>", "I-<institution>", "<institution>", "<institution>");
+        List<LayoutToken> tokenizations = this.analyzer.getLayoutTokensForTokenizedText(tokens);
+        List<String> affiliationBlocks = AffiliationAddressParser.getAffiliationBlocks(tokenizations);
+        String header = FeaturesVectorAffiliationAddress.addFeaturesAffiliationAddress(
+            affiliationBlocks, Arrays.asList(tokenizations), NO_PLACES_POSITIONS
+        );
+        String labelResult = addLabelsToFeatures(header, labels);
+        LOGGER.debug("labelResult: {}", labelResult);
+        List<Affiliation> result = this.target.resultBuilder(
+            labelResult,
+            tokenizations,
+            NO_USE_PRELABEL
+        );
+        assertThat("should have one affiliation", result, is(hasSize(1)));
+        Affiliation affiliation = result.get(0);
+        assertThat("institution.marker", affiliation.getMarker(), is("1"));
+        assertThat(
+            "institution.institutions",
+            affiliation.getInstitutions(),
+            is(Arrays.asList("University of Science"))
+        );
+    }
 }
